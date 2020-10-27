b00fca63310ff589bccac8ce9579a2222864994d
Bhavesh Saini <bsaini117@gmail.com>
2020-10-27 19:13:00 -0400

    Changed the hello variable to function and added function good_bye

--- new_prac_file.py
+++ new_prac_file.py
@@ -1,2 +1,8 @@
+def hello_func():
 greeting = "Welcome to a new repository"
-print(greeting)
+return (greeting)
+
+def goodbye_func():
+    goodbye = "See you soon on this repository"
+    return goodbye
+
